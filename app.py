f7b207dcb6d2d6bfd8079431e7dde911ebfb812f
maliabarker <89502882+maliabarker@users.noreply.github.com>
2021-12-23 13:32:15 -1000

    working on querying db

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+from flask_pymongo import pymongo
+import datetime
+
+CONNECTION_STRING = 'mongodb+srv://maliabarker:tpofbawf@cluster0.hllse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_default_database()
+events = db.events
 
 app = Flask(__name__)
 
@@ -13,6 +20,10 @@ def calendar():
 @app.route('/events')
 def events_index():
     return render_template('events.html')
+
+def find_closest_date():
+    events.find({'date': $gte: 25})
+    # events.find( {{'date': { $gte: '2021, 12, 23' } }} )
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
